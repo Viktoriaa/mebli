1f1a783c60eabd9a9eef30b28729fe690315cdae
Dmitry Chabanenko <chdn6026@mail.ru>
2013-05-22 15:19:02 +0300

    Changed the encoding

--- mebls.php
+++ mebls.php
@@ -1,4 +1,10 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
 <?php
+
 $mebly=$_REQUEST['mebly'];
 echo "mebly=$mebly <br>";
 $tzvet=$_REQUEST['tzvet'];
@@ -11,3 +17,5 @@ fputs($fp,"$mebli\t$tzvet\t$kilkist\n");
 fclose($fp);
 
 ?>
+</body>
+</html>
--- mmer.php
+++ mmer.php
@@ -1,7 +1,7 @@
 <html>
+
 <head>
-<meta Http-equiv "Content-Type"
-content="text/html<charset=windows-1251">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Замовл</title>
 </head>
 <body>
