4562ed767f31cab6646b7e7a3ba8eaea923c9c8d
Viktoriaa <viktoria_hop@mail.ru>
2013-05-24 14:26:21 +0300

    Update mmer.php

--- mmer.php
+++ mmer.php
@@ -13,17 +13,20 @@ header('Content-type: text/html; charset=utf-8');
 <H1>замовлення меблів</H1>
 <table border=2>
 <TR><td>mebly</td><td>tzvet</td><td>kilkist</td></tr>
-
+<?php
 $fp=fopen("zam.txt","r");
-if(fp){while(!feof($fp))
+if($fp)
+{while(!feof($fp))
 {
 
-stroka=fscanf($fp,"%s\t%s\t%s\d");
-list($mebly,$tzvet,kilkist)=stroka;
+$stroka=fscanf($fp,"%s\t%s\t%s\t%d\n");
+list($mebly,$tzvet,$kilkist)=$stroka;
 //$kilkist=$fscanf($fp,"%d\t");
 echo "<TR><td>$mebly</td> <td>$tzvet</td> ><td>$kilkist</td></TR>";
 //echo "";
 //echo "<TR><td>$kilkist</td></TR>";
-}fclose($fp);}
+}
+fclose($fp);}
+?>
 </body>
 </html>
